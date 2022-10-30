29b349bd035aa6f9174572bd3b7009d603c1f941
aljubuncic <aljubuncic1@etf.unsa.ba>
2022-10-30 20:28:27 +0100

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -0,0 +1,20 @@
+package ba.unsa.etf.rpr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Sat s= new Sat(15,30,45);
+        System.out.println(s);
+        s.Sljedeci();
+        System.out.println(s);
+        s.PomjeriZa(-48);
+        System.out.println(s);
+        s.Postavi(0,0,0);
+        System.out.println(s);
+    }
+}
